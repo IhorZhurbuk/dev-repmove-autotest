f8b32d33c8d48e2e86674f4b448995e412d58e23
Storage <Storage@DESKTOP-8IRRAG0>
2025-09-30 19:52:38 +0300

    litle fix

--- Factories/DriverFactory.cs
+++ Factories/DriverFactory.cs
@@ -1,39 +1,48 @@
 ﻿using dev_repmove_autotest.Utils.Enum;
 using Microsoft.Playwright;
+using System.Runtime.InteropServices;
 
-namespace dev_repmove_autotest.Factories
+public static class DriverFactory
 {
-    public static class DriverFactory
+    public static async Task<(IBrowser Browser, IBrowserContext Context)> CreateBrowserAsync(
+        IPlaywright playwright,
+        EBrowserType browserType,
+        bool headless = true)
     {
-        public static async Task<(IBrowser Browser, IBrowserContext Context)> CreateBrowserAsync(IPlaywright playwright, EBrowserType browserType, bool headless = true)
+
+        var args = new List<string>
         {
-            var launchOptions = new BrowserTypeLaunchOptions
-            {
-                Headless = headless,
-                Args = new[] {
-                    "--start-maximized",
-                    "--window-size=1920,1080",
-                    "--window-position=0,0"
-                }
-            };
+            "--window-size=1920,1080"
+        };
 
-            var browser = browserType switch
-            {
-                EBrowserType.Chromium => await playwright.Chromium.LaunchAsync(launchOptions),
-                EBrowserType.Firefox => await playwright.Firefox.LaunchAsync(launchOptions),
-                EBrowserType.WebKit => await playwright.Webkit.LaunchAsync(launchOptions),
-                _ => throw new ArgumentOutOfRangeException(nameof(browserType), $"Not suported browser: {browserType}")
-            };
-
-            var context = await browser.NewContextAsync(new BrowserNewContextOptions
-            {
-                ViewportSize = new ViewportSize { Width = 1920, Height = 1080 },
-                ScreenSize = new ScreenSize { Width = 1920, Height = 1080 },
-                HasTouch = false,
-                IsMobile = false
-            });
-
-            return (browser, context);
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            args.Add("--start-maximized");
+            args.Add("--window-position=0,0");
         }
+
+        var launchOptions = new BrowserTypeLaunchOptions
+        {
+            Headless = headless,
+            Args = args.ToArray()
+        };
+
+        var browser = browserType switch
+        {
+            EBrowserType.Chromium => await playwright.Chromium.LaunchAsync(launchOptions),
+            EBrowserType.Firefox => await playwright.Firefox.LaunchAsync(launchOptions),
+            EBrowserType.WebKit => await playwright.Webkit.LaunchAsync(launchOptions),
+            _ => throw new ArgumentOutOfRangeException(nameof(browserType), $"Not supported browser: {browserType}")
+        };
+
+        var context = await browser.NewContextAsync(new BrowserNewContextOptions
+        {
+            ViewportSize = new ViewportSize { Width = 1920, Height = 1080 },
+            ScreenSize = new ScreenSize { Width = 1920, Height = 1080 },
+            HasTouch = false,
+            IsMobile = false
+        });
+
+        return (browser, context);
     }
 }
--- README.md
+++ README.md
@@ -1,87 +1,64 @@
 
 ### **INSTALL SDK IF MISSING**
-
+Recomended os: Windows 10/11 
   - **Windows**: Download and install from [.NET Download](https://dotnet.microsoft.com/en-us/download/dotnet/8.0)
-  - **Linux (Ubuntu)**:
-
-<!-- end list -->
-
-```bash
-wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh -O dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh --version 8.0.100
-```
+  - **Linux (Ubuntu)**:   
+	- sudo apt update
+	- sudo apt install dotnet8
+ 
 
 ### **INSTALL ALLURE IF MISSING**
+	https://allurereport.org/docs/install/
 
-**Windows (using Scoop or Chocolatey):**
-
-```bash
-scoop install allure
-# or
-choco install allure
-```
-
-**Linux (Ubuntu):**
-
-```bash
-sudo apt-add-repository ppa:qameta/allure
-sudo apt-get update
-sudo apt-get install allure
-```
-
------
 
 ### **Setup and Execution**
 
 **1. Check .NET version**
 
-```bash
 dotnet --version # Should be >= 8.0
-```
+
 
 **2. Check Git version**
 
-```bash
 git --version
-```
+
 
 **3. Clone the project**
 
-```bash
+
 git clone <repo-url>
 cd dev-repmove-autotest
-```
+
 
 **4. Install packages**
 
-```bash
+
 dotnet restore
-```
+
 
 **5. Build the project**
 
-```bash
+
 dotnet build
-```
+
 
 **6. Install browsers**
 
-```bash
+
 pwsh bin/Debug/net8.0/playwright.ps1 install --with-deps
-```
+
 
 **7. Check Allure (optional)**
 
-```bash
+
 allure --version
-```
+
 
 **8. Run tests**
 
-```bash
+
 dotnet test --configuration Debug
-```
+
 
 -----
 
@@ -89,34 +66,32 @@ dotnet test --configuration Debug
 
 **1. Generate the report**
 
-```bash
+
 allure generate --clean "bin\Debug\net8.0\allure-results" -o "bin\Debug\net8.0\allure-report"
-```
+
 
 **2. Open the report**
 
-```bash
-allure open "bin\Debug\net8.0\allure-report"
-```
 
------
+allure open "bin\Debug\net8.0\allure-report"
+
+
 
 ### **Running Tests for Specific Browsers**
 
 **Only Chromium (Chrome/Edge)**
 
-```bash
 dotnet test --filter "FullyQualifiedName~Chromium"
-```
+
+
 
 **Only Firefox**
 
-```bash
+
 dotnet test --filter "FullyQualifiedName~Firefox"
-```
+
 
 **Only WebKit (Safari)**
 
-```bash
+
 dotnet test --filter "FullyQualifiedName~WebKit"
-```
